ddbc28320b33fdd29e3372157e46efb4fd41110c
Gabriela Maiara <gabrielamaiara14@gmail.com>
2022-08-21 16:27:48 -0300

    Commit 3

---
+++ aula4-tipos-de-dados/exercicio-type.py
@@ -0,0 +1,11 @@
+# String: nome
+print('Gabriela', type('Gabriela'))
+
+# Idade: int
+print('27', type(int('27')))
+
+# Altura: float
+print('1.65', type(float('1.65')))
+
+# É maior de idade
+print(27 >= 18, type(bool(27 >= 18)))
---
+++ aula4-tipos-de-dados/type.py
@@ -0,0 +1,16 @@
+"""
+Tipos de dados
+str - string
+int - inteiro
+float - real/ponto flutuante
+bool - booleano/lógico
+"""
+
+print('Gabriela', type('Gabriela'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(50 == 50, type(50 == 50))
+print(bool(''))
+
+print('Gabriela', type('Gabriela'), bool('Gabriela'))
+print('10', type('10'), type(int('10')))  # Conversão de tipo
