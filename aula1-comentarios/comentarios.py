20d098b6cb1e6edf089f87a2ad57f28c0379903d
Gabriela Maiara <gabrielamaiara14@gmail.com>
2022-08-21 16:13:22 -0300

    Commit 1

---
+++ aula1-comentarios/comentarios.py
@@ -0,0 +1,10 @@
+print('Linha 1')  # Outro comentário
+print('Linha 2')
+# Falando sobre do que se trata sessa linha
+print('Linha 3')
+print('Linha 4')  # Isso é um comentário
+'''
+print('Linha 5')
+print('Linha 6')
+'''
+# Final
