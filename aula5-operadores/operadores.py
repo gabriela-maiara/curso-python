cb258bd3a88a0feabd6c2316544cb89f144abeef
Gabriela Maiara <gabrielamaiara14@gmail.com>
2022-08-21 16:32:32 -0300

    Commit 4

---
+++ aula5-operadores/operadores.py
@@ -0,0 +1,16 @@
+'''
+x, -, *, /, //, **, %, ()
+'''
+
+print('Multiplicação: ', 10 * 10)
+print('Adição: ', 10 + 10)
+print('Subtração: ', 10 - 5)
+print('Divisão: ', 10 / 2)
+
+print('Multiplicação ', 5 * '10')  # Repetição
+print('Concatenação ', '5' + '5')
+print(10.5 // 3.5, 'Divisao inteira')
+print('Potenciação: ', 2 ** 10)
+print('Resto da divisão: ', 11 % 2)
+print(5+2*10)
+print((5+2)*10)
