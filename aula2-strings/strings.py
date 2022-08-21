5a327e98ba51443a1615dd572a69e219a59fe490
Gabriela Maiara <gabrielamaiara14@gmail.com>
2022-08-21 16:23:30 -0300

    Commit 2

---
+++ aula2-strings/strings.py
@@ -0,0 +1,4 @@
+print(123456)
+print('Gabriela', 'Maiara')
+print('Gabriela', 'Maiara', sep='-', end='####')
+print('João', 'e', 'Maria', sep='-', end='')
---
+++ aula3-string-parte2/string.py
@@ -0,0 +1,14 @@
+'''
+str - string
+'''
+
+print('alguma coisa')
+print("Aspas duplas")
+print('123456')  # String
+# print('Essa é um 'string' (str).')
+print("Essa é um 'string' (str).")
+print('Essa é um "string" (str).')
+print("Esse é meu \"texto\" (str).")
+print("Esse é meu \"texto\" (str).")
+print('Esse é meu \n (str).')
+print(r'Esse é meu \n (str).')
